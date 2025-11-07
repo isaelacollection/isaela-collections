9dbc7d130eddf6feef9b6cdb0a2c297d94a640cf
isaelacollection <isaelacollection@gmail.com>
2025-11-07 00:54:48 -0500

    arreglo del codigo fuente texto en products list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import ProductList from "../components/productList";
 const Home = () => {
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4 text-gray-800">Productos Disponibles</h2>
+     {/** <h2 className="text-2xl font-bold mb-4 text-gray-800">Productos Disponibles</h2> */} 
       <ProductList />   {/*el metodo que esta en la clase productList */}
     </div>
   );
