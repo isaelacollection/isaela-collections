8cf6ffd049d522527cf7287e482a70a43a17723f
isaelacollection <isaelacollection@gmail.com>
2025-12-07 14:24:59 -0500

    Actualización: creacion de un indice principal5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import ProductList from "../components/productList";
+
 import IndiceDeProductos from "../components/IndiceDeProductos";
 
 
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
      {/** <h2 className="text-2xl font-bold mb-4 text-gray-800">Productos Disponibles</h2> */} 
-    {/*  <ProductList />   {/*el metodo que esta en la clase productList */}
+   
     <IndiceDeProductos />
     </div>
   );
