25b37552ffa974830cfcbbea6925c863923fa2cc
isaelacollection <isaelacollection@gmail.com>
2025-12-14 16:03:46 -0500

    actualizar movil3

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,20 +3,27 @@ import Sidebar from "../components/Sidebar";
 
 const AdminLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen overflow-x-hidden">
+    <div className="flex flex-col min-h-screen">
 
-    <div className="flex h-screen">
-      {/* Sidebar fijo a la izquierda */}
-      <div className="w-60 bg-[#f1f0e7] text-black flex flex-col p-4">
-        <Sidebar />
+      {/*
+        ✅ CAMBIO CLAVE: Usar flex-col por defecto (móvil) y flex-row a partir de 'md' (escritorio).
+        Esto apila el menú y el contenido en el celular.
+        */}
+      <div className="flex flex-col md:flex-row min-h-screen">
+
+        {/* Sidebar: En móvil (w-full), toma todo el ancho. 
+                En escritorio (md:w-60), toma el ancho fijo de 240px.
+            */}
+        <div className="w-full md:w-60 bg-[#f1f0e7] text-black flex flex-col p-4">
+          <Sidebar />
+        </div>
+
+        {/* Contenido principal dinámico */}
+        <main className="flex-1 p-6 overflow-y-auto bg-white">
+          <Outlet />
+        </main>
       </div>
-
-      {/* Contenido principal dinámico */}
-      <main className="flex-1 p-6 overflow-y-auto bg-white">
-        <Outlet />
-      </main>
     </div>
-     </div>
   );
 };
 
